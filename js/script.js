7a4f31bed54504cffa7e534686d900b1d2ac0cf9
Stefano Patti <mastepa@hotmail.com>
2021-03-23 19:05:23 +0100

    chiusura al click
    uso non corretto di stop propagation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 function onButton(){
-  // $('.hamburger-menu').addClass('active');
-  console.log('sei sopra');
 
   $('.hamburger-menu.active').removeClass('active');
+
   $(this).next().addClass('active');
-
-
 }
+
+function bodyClick(event){
+
+  $('.hamburger-menu.active').removeClass('active');  
+
+  event.stopPropagation();
+}
+
+
 
 function init(){
   $('.menu').hover(onButton);
+
+  $('body').click(bodyClick);
 }
 
 $(document).ready(init);
